7972ad03cc601c83c2ec3e978e3e73ab267680a7
SAURAV-8968 <119794270+SAURAV-8968@users.noreply.github.com>
2024-01-18 19:13:12 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="mailto:sonisaurav211@gmail.com"><i class="fas fa-envelope"></i><span>sonisaurav211@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>pandher , amritsar , punjab</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <!-- <a href="saved.php"><span>saved properties</span></a> -->
+      </div>
+
+      <div class="box">
+         <a href="#"><i class="fab fa-facebook-f"></i> <span>facebook</span></a>
+         <a href="#"><i class="fab fa-twitter"></i> <span>twitter</span></a>
+         <a href="#"><i class="fab fa-linkedin"></i> <span>linkedin</span></a>
+         <a href="#"><i class="fab fa-instagram"></i> <span>instagram</span></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. saurav soni </span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
